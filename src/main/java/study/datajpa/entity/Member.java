9ad5a24b83c27c45176f4dffe1b0da06eb1d25b7
KwangHo <skh990427@naver.com>
2023-09-05 22:02:21 +0900

    예제 도메인 모델 및 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,47 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) //연관관계는 무한루프때문에 @ToString 안하는게 좋다
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
-    }
+//    @NoArgsConstructor 때문에 필요없음
+//    protected Member() {
+//    }
 
     public Member(String username) {
         this.username = username;
     }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team; //내 팀을 바꾸고
+        team.getMembers().add(this); //연관관계 팀에 추가 해줘야함
+    }
+
+
 }
