60e1a593217b7cae4041b954fa4902534024d8f1
KwangHo <skh990427@naver.com>
2023-09-10 18:16:28 +0900

    @Query, DTO 값 조회

---
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -0,0 +1,16 @@
+package study.datajpa.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDto {
+    private Long id;
+    private String name;
+    private String teamName;
+
+    public MemberDto(Long id, String name, String teamName) {
+        this.id = id;
+        this.name = name;
+        this.teamName = teamName;
+    }
+}
--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -20,5 +21,13 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     //실무에서 자주 사용됨
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUserNameList();
+
+    //Dto 조회를 할때는 생성자 생성하듯이 경로를 다적어줘야함.. 조금 복잡하다
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
 
 }
