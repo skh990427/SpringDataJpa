dfe4e06daf2f14b0ec1d04dc4980a36ad9c60c27
KwangHo <skh990427@naver.com>
2023-09-12 08:38:53 +0900

    순수 JPA 벌크성 수정 쿼리

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -73,4 +73,10 @@ public class MemberJpaRepository {
                 .getSingleResult();
 
     }
+
+    public int bulkAgePlus(int age) {
+        return em.createQuery("update Member m set m.age = m.age + 1 where m.age >= :age")
+                .setParameter("age", age)
+                .executeUpdate(); //executeUpdate 응답값의 개수가 출력됨
+    }
 }
